24ef2711ec7cada859cd751839849e0827c05ee1
NaregB <nareg111@hotmail.com>
2024-03-18 22:49:29 +0400

    Rename v1/user to v1/users

--- presentation/presentation-impl/src/main/java/am/cs322/UserControllerImpl.java
+++ presentation/presentation-impl/src/main/java/am/cs322/UserControllerImpl.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/user")
+@RequestMapping("/v1/users")
 public class UserControllerImpl implements UserController {
 
     private final UserService userService;
